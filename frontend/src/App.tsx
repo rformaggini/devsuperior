bc666511de3e3740166ac6f9a2c5b12208069b96
Rodney Formaggini <rformaggini@gmail.com>
2021-05-08 00:01:20 -0300

    Create charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import BarChart from "components/BarChart";
 import DataTable from "components/DataTable";
+import DonutChart from "components/DonutChart";
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
 
@@ -7,7 +9,25 @@ function App() {
     <>
       <NavBar />
       <div className="container">
-        <h1 className="text-primary">OLA Mundo </h1>
+        <h1 className="text-primary py-3">DashBoard Relatórios</h1>
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">
+              Taxa de sucesso (%)
+            </h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">
+              Total de vendas
+            </h5>
+            <DonutChart />
+          </div>
+        </div>
+      <div className="py-3">
+        <h2 className="text-primary">Total de Vendas</h2>
+      </div>
         <DataTable />
       </div>
       <Footer />
