4177417faa3d162f7ee88629fc57c0eea5190079
Rodney Formaggini <rformaggini@gmail.com>
2021-05-07 23:31:52 -0300

    create footer and navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,15 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
 function App() {
   return (
-    <div>
+    <>
+    <NavBar />
+    <div className="container">
       <h1 className="text-primary">OLA Mundo </h1>
     </div>
+    <Footer />
+    </>
   );
 }
 
