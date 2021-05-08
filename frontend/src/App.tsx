2ed259b0219f04f8d80451aef15858e77d614a90
Rodney Formaggini <rformaggini@gmail.com>
2021-05-07 23:35:58 -0300

    create component datatable

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,16 @@
+import DataTable from "components/DataTable";
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
 
 function App() {
   return (
     <>
-    <NavBar />
-    <div className="container">
-      <h1 className="text-primary">OLA Mundo </h1>
-    </div>
-    <Footer />
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">OLA Mundo </h1>
+        <DataTable />
+      </div>
+      <Footer />
     </>
   );
 }
